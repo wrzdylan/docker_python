965c1641bdf3c975fe5b23abf0acd0b5a4119484
Dylan WRZESNIEWSKI <wrzdylan@gmail.com>
2022-12-08 16:24:36 +0100

    add adminer

--- python/app/main.py
+++ python/app/main.py
@@ -1,8 +1,25 @@
+import psycopg2
+import os
 from typing import Union
 
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
 
 app = FastAPI()
+
+
+def db_conn():
+    conn = psycopg2.connect(
+        dbname=os.getenv("POSTGRES_DB", "postgres"), 
+        user="postgres", 
+        password=os.getenv("POSTGRES_PASSWORD", "secret"),
+        host="test",
+    )
+
+    print("Database connected successfully")
+
+    return conn
 
 
 @app.get("/")
@@ -13,3 +30,5 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
